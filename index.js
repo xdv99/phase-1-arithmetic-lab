06d001da2a0ce7328e20ba252de1b62a29ed1152
Dawood Alkawaz <dalkawaz@gmail.com>
2024-02-19 17:18:02 +0300

    Solution

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 9;
+const num4 = 5;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 9 % 5;
+
+const max = Math.max(1, 2, 6, 20)
+
+
+console.log(mod)
